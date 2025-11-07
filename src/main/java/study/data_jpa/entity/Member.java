212f787965c341f1c4a248f01525c56e16e84352
tjdgus903 <tjdgusgus123@gmail.com>
2025-11-07 15:43:17 +0900

    insert member service

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -1,25 +1,43 @@
 package study.data_jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name="member_id")
     private Long id;
     private String username;
+    private int age;
 
-    // jpa Entity 는 기본적으로 생성자가 하나 있어야됨(파라미터 없이)
-    protected Member(){
-    }
+    // Member는 여러명이고 소속되는 팀은 하나이기 때문에 N대 1 - ManyToOne
+    // Many이기 때문에 one 에 대한 컬럼을 조인해야됨
+    // 실무에서는 지연 로딩으로 설정해야됨(조회 시 db 조회)
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username){
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team){
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
---
+++ src/main/java/study/data_jpa/entity/Team.java
@@ -0,0 +1,29 @@
+package study.data_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","name"})
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    // 팀은 하나이고 member는 N이기 때문에 1대 다(항상 선언주체가 앞으로) - OneToMany
+    // 외래키가 없는 쪽에 mappedBy를 지정해야됨
+    // 현재 member쪽에서 team 을 참조하기 때문에 외래키는 member
+    @OneToMany(mappedBy = "team")
+    private List<Member> members=new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
