dd0773a7e84112b73366353ea2b903b1d35ef857
tjdgus903 <tjdgusgus123@gmail.com>
2025-11-13 07:57:18 +0900

    순수 JPA 페이징 및 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -50,4 +50,18 @@ public class MemberJpaRepository {
                 .setParameter("age",age)
                 .getResultList();
     }
+
+    public List<Member> findByPage(int age, int offset, int limit){
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset) // 몇번째인지
+                .setMaxResults(limit)   // 값을 몇개 가져올건지
+                .getResultList();
+    }
+
+    public long totalCount(int age){
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
