2f41a9a4179428045bee52ba5622d458e4500d0e
tjdgus903 <tjdgusgus123@gmail.com>
2025-12-07 20:32:57 +0900

    Spring JPA Auditing

--- src/test/java/study/datajpa/entity/TeamTest.java
+++ src/test/java/study/datajpa/entity/TeamTest.java
@@ -71,6 +71,8 @@ class TeamTest {
 
         //then
         System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
-        System.out.println("findMember.updatedDate = " + findMember.getUpdatedDate());
+        System.out.println("findMember.updatedDate = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.createdBy = " + findMember.getCreatedBy());
+        System.out.println("findMember.updatedBy = " + findMember.getLastModifiedBy());
     }
 }
